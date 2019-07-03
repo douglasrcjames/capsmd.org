ea024c29f4957231a710cb7a444ed1a2f91ff5a6
Douglas James <douglasrcjames@gmail.com>
2019-07-03 09:06:59 -0700

    BRT Link changes and dashes

--- src/components/issues/InfrastructureScreens/StoriesOpinions/HowToDoBRTProperly.js
+++ src/components/issues/InfrastructureScreens/StoriesOpinions/HowToDoBRTProperly.js
@@ -8,9 +8,9 @@ export default class HowToDoBRTProperly extends Component {
        
         <p>
             For over a decade, Montgomery County has been studying bus rapid transit, a 
-            kind of bus ser-vice that’s faster and more reliable. Construction is underway 
+            kind of bus service that’s faster and more reliable. Construction is underway 
             on the first phase of what could be a countywide system: Flash, an 11-mile line 
-            between downtown Silver Spring and Bur-tonsville. Meanwhile, across the Potomac 
+            between downtown Silver Spring and Burtonsville. Meanwhile, across the Potomac 
             River, two Northern Virginia communities funded, built, and are now operating a 
             BRT line of their own during the same amount of time. What can we learn from them?
         </p>
@@ -27,54 +27,105 @@ export default class HowToDoBRTProperly extends Component {
             
         <p>
             That is, unless you’re riding the bus. Down the middle of Route 1 is a wide, lushly landscaped median 
-            with two transit-only lanes, where bright blue Metroway buses hustle through the con-gestion on either 
+            with two transit-only lanes, where bright blue Metroway buses hustle through the congestion on either 
             side. At stoplights, a special signal lets the bus pass ahead of the adjacent cars. 
-            Every few blocks, they stop at matching blue structures with curved roofs, collecting pas-sengers who 
+            Every few blocks, they stop at matching blue structures with curved roofs, collecting passengers who 
             queue up at bumpy sidewalk tiles you might recognize from Metro stations.
         </p>
             
         <p>
             Grab one of these buses and it will take you north into Arlington County, where the transit lanes veer off 
-            Richmond Highway and past new office buildings hosting the offices of compa-nies like Lidl, Bloomberg BNA, 
+            Richmond Highway and past new office buildings hosting the offices of companies like Lidl, Bloomberg BNA, 
             and the National Industries for the Blind. It’s not long until you reach Crystal City and Pentagon City, the northern terminus of the line, 
             where this fall Ama-zon’s new headquarters will open for business. The five-mile trip takes just 24 minutes, 
             one-quarter less than local buses serving the same corridor.
         </p>
 
         <p>
+            This is the vision the city of Alexandria and Arlington County first laid out almost 20 years ago - 
             <a 
                 href="https://ggwash.org/view/37881/a-history-of-streetcar-planning-in-northern-virginia"
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                &nbsp;This is the vision the city of Alexandria and Arlington County first laid out almost 20 years ago&nbsp;
+                &nbsp;a new transit line,&nbsp;
             </a>
-            - a new transit line, running on the street, to support both communities’ fastest-growing neigh-borhoods and 
-            connect them to the Metro system. 
+            running on the street, to support both communities’ fastest-growing neighborhoods and 
+            connect them to the Metro system. In 2010, shortly after Montgomery County began studying what
             <a 
                 href="https://projects.arlingtonva.us/projects/crystal-city-potomac-yard-transitway/"
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                &nbsp;In 2010, shortly after Montgomery County began studying what bus rapid transit could even look like&nbsp;
+                &nbsp;bus rapid transit&nbsp;
             </a>
-            the two jurisdictions kicked off 
+            could even look like the two jurisdictions kicked off 
             planning and design work on what became Metroway, and the five-mile line opened in the summer of 2015.
         </p>
 
         <p>
-            At first, the line got off to a rocky start. Some residents doubted the line’s potential (https://alextimes.com/2015/03/your-view-metroway-is-yet-another-waste-of-taxpayer-dollars/), 
-            and Metro even considered shutting Metroway down a year after it opened (https://ggwash.org/view/43267/wmata-is-considering-scrapping-the-metroway-brt).
+            At first, the line got off to a 
+            <a 
+                href="https://alextimes.com/2015/03/your-view-metroway-is-yet-another-waste-of-taxpayer-dollars/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;rocky start.&nbsp;
+            </a>
+            Some residents doubted the line’s potential, 
+            and Metro even considered 
+            <a 
+                href="https://ggwash.org/view/43267/wmata-is-considering-scrapping-the-metroway-brt"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;shutting Metroway down&nbsp;
+            </a>
+            a year after it opened.
         </p>
         <p>
-            It didn’t take long for the line to exceed expectations. Despite an overall decline in DC area bus ridership, 
-            (https://www.washingtonpost.com/news/dr-gridlock/wp/2018/07/06/with-ridership-falling-metro-will-spend-2-2-million-to-study-bus-business-model/?utm_term=.fb563c1246eb) 
-            an average of 2400 people rode Metroway each day in 2018 (https://www.alexandriava.gov/tes/info/default.aspx?id=58644) 50% more than the local bus it replaced, and 
-            more than planners anticipated (https://www.washingtonpost.com/local/trafficandcommuting/metroway-the-regions-first-bus-rapid-transit-debuts-sunday-in-northern-virginia/2014/08/22/17c63392-2920-11e4-958c-268a320a60ce_story.html?utm_term=.261ca82f9cdf). 
+            It didn’t take long for the line to 
+            <a 
+                href="https://www.washingtonpost.com/news/dr-gridlock/wp/2018/07/06/with-ridership-falling-metro-will-spend-2-2-million-to-study-bus-business-model/?utm_term=.fb563c1246eb"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;exceed expectations.&nbsp;
+            </a>
+            Despite an overall decline in DC area bus ridership, an average of 
+            <a 
+                href="https://www.alexandriava.gov/tes/info/default.aspx?id=58644"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;2400 people rode Metroway each day in 2018,&nbsp;
+            </a>
+            50% more than the local bus it replaced, and more than 
+            <a 
+                href="https://www.washingtonpost.com/local/trafficandcommuting/metroway-the-regions-first-bus-rapid-transit-debuts-sunday-in-northern-virginia/2014/08/22/17c63392-2920-11e4-958c-268a320a60ce_story.html?utm_term=.261ca82f9cdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;planners anticipated.&nbsp;
+            </a>
             New residents and businesses flocked to the area: hundreds of new homes and 
-            thousands of square feet of office space has been built around Metroway stations, and local apartment buildings market their proximity to 
-            the line (http://www.liveatnotch8.com/neighborhood/). And Arlington County is extending the line fur-ther north, to Pentagon City 
-            (https://www.arlnow.com/2018/11/08/as-pentagon-city-transitway-expansion-gains-steam-county-convenes-open-house-tonight/). 
+            thousands of square feet of office space has been built around Metroway stations, and 
+            <a 
+                href="http://www.liveatnotch8.com/neighborhood/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;local apartment buildings&nbsp;
+            </a>
+            market their proximity to the line. And Arlington County is 
+            <a 
+                href="https://www.arlnow.com/2018/11/08/as-pentagon-city-transitway-expansion-gains-steam-county-convenes-open-house-tonight/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;extending the line further north,&nbsp;
+            </a>
+            to Pentagon City.
         </p>
             
         <h2>WHAT DID WE LEARN</h2>
@@ -92,17 +143,25 @@ export default class HowToDoBRTProperly extends Component {
         <p>
             The other lesson is that building good transit can be challenging, but is worth the trouble. Metroway’s 
             original phase cost $43 million to build; Arlington and Alexandria paid for it with state and local tax revenue, 
-            a federal TIGER grant for small, shovel-ready transportation pro-jects to build Metroway, and some money from 
-            nearby developers (https://www.mwcog.org/about-us/newsroom/2014/09/02/metroway-region-s-first-brt-line-gets-start-with-tiger-help-alexandria-arlington-county-bus-rapid-transit-bus-priority-tiger/). Both 
-            communities set aside space for bus lanes years in advance, whether by repurposing car lanes for buses, or 
-            by requiring property owners to set aside land next to the street for new bus lanes. 
+            a federal 
+            <a 
+                href="https://www.mwcog.org/about-us/newsroom/2014/09/02/metroway-region-s-first-brt-line-gets-start-with-tiger-help-alexandria-arlington-county-bus-rapid-transit-bus-priority-tiger/"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;TIGER&nbsp;
+            </a>
+            grant for small, shovel-ready transportation projects to build Metroway, and some money from 
+            nearby developers. Both communities set aside space for bus lanes years in advance, 
+            whether by repurposing car lanes for buses, or by requiring property owners to set 
+            aside land next to the street for new bus lanes. 
         </p>
         
         <p>
             Raising the money and getting the land to build Metroway were both politically difficult. just like in 
             Montgomery County, residents were reluctant to spend the money or reconfigure streets in an area that 
-            already has traffic congestion. While Northern Virginia choose to build dedicat-ed bus lanes, Montgomery 
-            County chose not to build dedicated lanes through the most congest-ed portion of the Flash corridor, 
+            already has traffic congestion. While Northern Virginia choose to build dedicated bus lanes, Montgomery 
+            County chose not to build dedicated lanes through the most congested portion of the Flash corridor, 
             though buses will get to use the shoulders in other areas. 
         </p>
         
@@ -114,11 +173,25 @@ export default class HowToDoBRTProperly extends Component {
         </p>
         
         <p>
-            And by contributing to the kind of transit-rich, urban community that people and employers increasingly demand, the line has 
-            helped draw thousands of new homes and hundreds of thou-sands of square feet of office space 
-            (https://www.bisnow.com/washington-dc/news/mixed-use/a-look-at-the-massive-development-pipeline-around-the-upcoming-potomac-yard-metro-station-67248), 
-            representing billions of dollars of private investment. Amazon’s arrival will bring another $2.5 billion in investment with it. 
-            (https://www.businessinsider.com/amazon-hq-arlington-virginia-national-landing-crystal-city-photos-2018-11#-including-a-130000-square-foot-shopping-and-entertainment-space-known-as-central-district-retail--4)
+            And by contributing to the kind of transit-rich, 
+            <a 
+                href="https://www.bisnow.com/washington-dc/news/mixed-use/a-look-at-the-massive-development-pipeline-around-the-upcoming-potomac-yard-metro-station-67248"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;urban community&nbsp;
+            </a>
+            that people and employers increasingly demand, the line has 
+            helped draw thousands of new homes and hundreds of thousands of square feet of office space, 
+            representing billions of dollars of private investment. 
+            <a 
+                href="https://www.businessinsider.com/amazon-hq-arlington-virginia-national-landing-crystal-city-photos-2018-11#-including-a-130000-square-foot-shopping-and-entertainment-space-known-as-central-district-retail--4"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                &nbsp;Amazon’s&nbsp;
+            </a>
+            arrival will bring another $2.5 billion in investment with it. 
         </p>
 
         <p>
